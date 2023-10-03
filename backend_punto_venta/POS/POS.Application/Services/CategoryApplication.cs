172e21397f5e8586d4cebd617e324792293b78d1
LucOrt91 <lucasnortega91@gmail.com>
2023-10-03 16:04:55 -0300

    clases clases bases, implementacion de interfaces y constantes

---
+++ backend_punto_venta/POS/POS.Application/Services/CategoryApplication.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using POS.Application.Commons.Bases;
+using POS.Application.Dtos.Request;
+using POS.Application.Dtos.Response;
+using POS.Application.Interfaces;
+using POS.Application.Validators.Category;
+using POS.Infrastructure.Commons.Bases.Request;
+using POS.Infrastructure.Commons.Bases.Response;
+using POS.Infrastructure.Persistences.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Application.Services
+{
+    public class CategoryApplication : ICategoryApplication
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly CategoryValidator _validationRules;
+
+        public CategoryApplication(IUnitOfWork unitOfWork, IMapper mapper, CategoryValidator validationRules)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _validationRules = validationRules;
+        }
+        public Task<BaseResponse<BaseEntityResponse<CategoyResponseDto>>> ListCategories(BaseFilterRequest filters)
+        {
+            throw new NotImplementedException();
+        } 
+        public Task<BaseResponse<IEnumerable<CategorySelectResponseDto>>> ListSelectCategories()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<BaseResponse<CategoyResponseDto>> CategoryById(int categoryId)
+        {
+            throw new NotImplementedException();
+        } 
+        public Task<BaseResponse<bool>> RegisterCategory(CategoyResponseDto responseDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<bool>> EditCategory(int categoryId, CategoryRequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }           
+      
+
+        public Task<BaseResponse<bool>> RemoveCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

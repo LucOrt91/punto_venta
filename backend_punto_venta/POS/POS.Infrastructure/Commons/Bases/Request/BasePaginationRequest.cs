be7c69157cd5fe4585750c94439909f4eea9ae98
LucOrt91 <lucasnortega91@gmail.com>
2023-10-03 14:38:08 -0300

    comienzo proyecto
    Conexion con base de datos, scaffolding, implementacion de patrones builder, repository y UnitOfWork

---
+++ backend_punto_venta/POS/POS.Infrastructure/Commons/Bases/Request/BasePaginationRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace POS.Infrastructure.Commons.Bases.Request
+{
+    public class BasePaginationRequest
+    {
+        public int NumPage { get; set; } = 1;
+        public int NumRecordsPage { get; set; } = 10;
+
+        private readonly int NumMaxRecords = 50;
+        public string Order { get; set; } = "asc";
+        public string? Sort { get; set; } = null;
+
+        public int Recods
+        {
+            get => NumMaxRecords;
+            set
+            {
+                NumRecordsPage = value > NumMaxRecords ? NumMaxRecords : value;
+            }
+        }
+
+    }
+}
